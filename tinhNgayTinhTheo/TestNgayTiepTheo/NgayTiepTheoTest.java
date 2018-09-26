48e53f50b9c2a448660503f7fe62af77b83ba65b
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-09-27 04:48:40 +0700

    Tinh Ngay Tiep Theo

---
+++ tinhNgayTinhTheo/TestNgayTiepTheo/NgayTiepTheoTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NgayTiepTheoTest {
+    @Test
+    void nextDay1() {
+        String s = "01/01/2018";
+
+        String expected = "02/01/2018";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void nextDay2() {
+        String s = "31/01/2018";
+
+        String expected = "01/02/2018";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    void nextDay3() {
+        String s = "30/04/2018";
+
+        String expected = "01/05/2018";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+
+
+    @Test
+    void nextDay4() {
+        String s = "28/02/2018";
+
+        String expected = "01/03/2018";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void nextDay5() {
+        String s = "29/02/2020";
+
+        String expected = "01/03/2020";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void nextDay6() {
+        String s = "31/12/2018";
+
+        String expected = "01/01/2019";
+        String result = NgayTiepTheo.nextDay(s);
+        assertEquals(expected, result);
+    }
+}
